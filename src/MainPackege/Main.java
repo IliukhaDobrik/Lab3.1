20b8d3daf916494f84cd490c1022f79c28606f03
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2021-11-20 21:43:47 +0300

    CREATED class GornerTableCellRenderer

--- src/MainPackege/Main.java
+++ src/MainPackege/Main.java
@@ -3,6 +3,24 @@ package MainPackege;
 public class Main {
 
     public static void main(String[] args) {
-        new MainFrame();
+        if (args.length == 0) {
+            System.out.println("Невозможно табулировать многочлен, для которого не задано ни одного коэффициента!");
+            System.exit(-1);
+        }
+
+        Double[] coefficient = new Double[args.length];
+
+        int i = 0;
+        try {
+            for (String arg : args) {
+                coefficient[i++] = Double.parseDouble(arg);
+            }
+        } catch (NumberFormatException ex) {
+            System.out.println("Ошибка преобразования строки '" +
+                    args[i] + "' в число типа Double");
+            System.exit(-2);
+        }
+
+        new MainFrame(coefficient);
     }
 }
